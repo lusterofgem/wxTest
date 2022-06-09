c07cf9fc1d3985e8a5995b587ae04514f88d7f86
kde <lusterofgem@gmail.com>
2022-06-10 01:55:00 +0800

    add comment

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,12 @@
 
 #include <wx/app.h>
 #include <wx/glcanvas.h>
-
 class wxTest : public wxApp
 {
     virtual bool OnInit() override
     {
+        // /usr/bin/ld: obj/main.o: undefined reference to symbol '_ZN9wxAppBase12SafeYieldForEP8wxWindowl@@WXU_3.0'
+        // /usr/bin/ld: /lib/x86_64-linux-gnu/libwx_gtk3u_core-3.0.so.0: error adding symbols: DSO missing from command line
         wxGLCanvas *canvas = new wxGLCanvas(nullptr, wxID_ANY, nullptr);
         canvas->Show();
         std::cout << "hello world\n";
