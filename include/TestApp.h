7bb81a52028e9c1ae7de87fd9044af1e83304ca2
kde <lusterofgem@gmail.com>
2022-07-20 16:06:12 +0800

    add a test button

--- include/TestApp.h
+++ include/TestApp.h
@@ -1,12 +1,16 @@
 #ifndef TESTAPP_H
 #define TESTAPP_H
 
-#include <wx/wx.h>
+#include <wx/app.h>
+#include <wx/button.h>
+#include <wx/frame.h>
+#include <wx/sizer.h>
 
 class TestApp : public wxApp
 {
-    wxFrame mainFrame;
-    wxBoxSizer mainFrameSizer;
+    wxFrame *mainFrame;
+    wxBoxSizer *mainFrameSizer;
+    wxButton *testButton;
 
 public:
     virtual bool OnInit();
--- src/TestApp.cpp
+++ src/TestApp.cpp
@@ -1,13 +1,24 @@
+#include <iostream>
+
 #include "TestApp.h"
 
 bool TestApp::OnInit()
 {
-    // mainFrame = new wxFrame(nullptr, wxID_ANY, "wxTest");
-    // mainFrameSizer = new wxBoxSizer(wxVERTICAL);
+    mainFrame = new wxFrame(nullptr, wxID_ANY, "wxTest");
+    mainFrameSizer = new wxBoxSizer(wxVERTICAL);
+    testButton = new wxButton(mainFrame, wxID_ANY, "test button");
 
-    // // mainFrame
-    // mainFrame->SetSizer(mainFrameSizer);
-    // mainFrame->Show();
+    // mainFrame
+    mainFrame->SetSizer(mainFrameSizer);
+    mainFrame->Show();
+
+    // mainFrameSizer
+    mainFrameSizer->Add(testButton);
+
+    // testButton
+    testButton->Bind(wxEVT_BUTTON, [](wxCommandEvent &commandEvent){
+        std::cout << "testButton clicked!\n";
+    });
 
     return true;
 }
